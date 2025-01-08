58718ce88242e6a95efc1eb76e1c01d2854da5b2
anudhull <dhullanu99@gmail.com>
2025-01-08 23:33:20 +0530

    Refactor code into smaller functions for clarity

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,21 +1,32 @@
 export class StringCalculator {
   add(input: string): number {
-    if (input === '') {
-      return 0;
-    }
-    const delimiters = [',', '\n'];
+    if (input === '') return 0;
 
-    if (input.startsWith('//')) {
-      const customDelimiter = input[2];
-      delimiters.push(customDelimiter);
-      input = input.slice(4);
-    }
-    
+    const { delimiters, processedInput } = this.extractDelimiters(input);
     let delimiterRegex = new RegExp(`[${delimiters.join("")}]`);
-    const numbers = input.split(delimiterRegex).map(num => this.parseStringToNumber(num));
+    const numbers = this.splitInput(processedInput, delimiterRegex);
+
     this.handleNegativeNumbers(numbers);
 
     return this.calculateSum(numbers);
+  }
+
+  private extractDelimiters(input: string): { delimiters: string[], processedInput: string } {
+    const defaultDelimiters = [',', '\n'];
+    let processedInput = input;
+
+    if (input.startsWith('//')) {
+      const customDelimiter = input[2];
+      const remainder = input.slice(4);
+      defaultDelimiters.push(customDelimiter);
+      processedInput = remainder;
+    }
+
+    return { delimiters: defaultDelimiters, processedInput };
+  }
+
+  private splitInput(input: string, delimiterRegex: RegExp): number[] {
+    return input.split(delimiterRegex).map(num => this.parseStringToNumber(num));
   }
 
   private parseStringToNumber(input: string): number {
